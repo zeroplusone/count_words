f3cdca40a7b965c457ea8942cf06f1d947b13655
zeroplusone <a711186@gmail.com>
2016-12-09 23:18:00 +0800

    remove symbols at the sentence end, reverse answer

--- count_words.py
+++ count_words.py
@@ -3,22 +3,34 @@ import sys
 import string
 import fileinput
 
+def process(word):
+    word = word.lower()
+    return word
+
 if __name__ == "__main__":
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     dic = {}
     with fileinput.input(files=input_file) as f:
         for line in f:
+            # to eliminate the symbol at the end of sentence
+            line = line[0:-2]
+            # eliminate dash
             sentence = re.split('-| |\n', line)
             for word in sentence:
-                word = word.lower()
+                # transfer to lowercase and eliminate symbols
+                word = process(word)
                 if bool(re.search(r'\d', word)):
+                    # remove number
                     continue
                 elif word == '':
+                    # remove empty string
                     continue
                 elif word in dic:
                     dic[word]=dic[word]+1
                 else:
                     dic[word]=1
-        for key, value in sorted(dic.items(), key=lambda x:(x[1],x[0])):
+        for key, value in sorted(dic.items(), key=lambda x:(x[1],x[0]), reverse=True):
             print(key, value)
+
+
